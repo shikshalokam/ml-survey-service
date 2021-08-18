00bc6c2e0f2dd721796c2e1d91a8076458a4b425
Priyanka Pradeep <priyanka@tunerlabs.com>
2021-08-18 18:52:34 +0530

    fix

--- config/globalVariable.js
+++ config/globalVariable.js
@@ -27,13 +27,6 @@ module.exports = function () {
     }
   });
 
-  // create report folder in public
-  let csvReportsPath = ROOT_PATH + "/public/reports" ;
-
-  if( !fs.existsSync(csvReportsPath)) {
-    fs.mkdirSync(csvReportsPath);
-  }
-
   //load base v1 controllers
   fs.readdirSync(ROOT_PATH + '/controllers/v1/').forEach(function (file) {
     checkWhetherFolderExistsOrNor(ROOT_PATH + '/controllers/v1/', file);
