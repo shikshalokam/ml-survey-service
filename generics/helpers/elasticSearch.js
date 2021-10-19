9e2956a40b792b81e4429d26ec28138ee7b8083d
Priyanka Pradeep <priyanka@tunerlabs.com>
2021-10-19 12:58:33 +0530

    Revert "remove elastic search"
    This reverts commit 5eb827b9820489edfc3f8629ddc4451ba2fe6712.

--- generics/helpers/elasticSearch.js
+++ generics/helpers/elasticSearch.js
@@ -1,103 +1,103 @@
-// /**
-//  * name : elasticSearch.js
-//  * author : Aman Jung Karki
-//  * created-date : 11-Jun-2020
-//  * Description : elastic search common functionality
-//  */
+/**
+ * name : elasticSearch.js
+ * author : Aman Jung Karki
+ * created-date : 11-Jun-2020
+ * Description : elastic search common functionality
+ */
 
-// /**
-//   * Get user data
-//   * @function
-//   * @name get
-//   * @param {String} id - a unique document id
-//   * @param {String} index - elastic search index
-//   * @param {String} type - elastic search type
-//   * @returns {Object} Get user data.
-// */
+/**
+  * Get user data
+  * @function
+  * @name get
+  * @param {String} id - a unique document id
+  * @param {String} index - elastic search index
+  * @param {String} type - elastic search type
+  * @returns {Object} Get user data.
+*/
 
-// var get = function ( 
-//   id = "",
-//   index = "",
-//   type = ""
-// ) {
+var get = function ( 
+  id = "",
+  index = "",
+  type = ""
+) {
 
-//   return new Promise(async function (resolve, reject) {
-//     try {
+  return new Promise(async function (resolve, reject) {
+    try {
 
-//       if (id == "") {
-//         throw new Error("ID is required");
-//       }
+      if (id == "") {
+        throw new Error("ID is required");
+      }
 
-//       if (index == "") {
-//         throw new Error("Index is required");
-//       }
+      if (index == "") {
+        throw new Error("Index is required");
+      }
 
-//       const result = await elasticsearch.client.get({
-//         id: id,
-//         index: index
-//       }, {
-//           ignore: [httpStatusCode["not_found"].status],
-//           maxRetries: 3
-//         });
+      const result = await elasticsearch.client.get({
+        id: id,
+        index: index
+      }, {
+          ignore: [httpStatusCode["not_found"].status],
+          maxRetries: 3
+        });
 
-//       return resolve(result);
+      return resolve(result);
 
-//     } catch (error) {
-//       return reject(error);
-//     }
-//   })
-// };
+    } catch (error) {
+      return reject(error);
+    }
+  })
+};
 
-// /**
-//   * Create or update based on user data.
-//   * @function
-//   * @name createOrUpdate
-//   * @param {String} id - a unique document id
-//   * @param {String} index - elastic search index
-//   * @param {String} data - requested data
-//   * @returns {Object} created or updated user data.
-// */
+/**
+  * Create or update based on user data.
+  * @function
+  * @name createOrUpdate
+  * @param {String} id - a unique document id
+  * @param {String} index - elastic search index
+  * @param {String} data - requested data
+  * @returns {Object} created or updated user data.
+*/
 
-// var createOrUpdate = function (
-//   id = "",
-//   index = "",
-//   data = ""
-// ) {
+var createOrUpdate = function (
+  id = "",
+  index = "",
+  data = ""
+) {
 
-//   return new Promise(async function (resolve, reject) {
-//     try {
+  return new Promise(async function (resolve, reject) {
+    try {
 
-//       if (id == "") {
-//         throw new Error("ID is required");
-//       }
+      if (id == "") {
+        throw new Error("ID is required");
+      }
 
-//       if (index == "") {
-//         throw new Error("Index is required");
-//       }
+      if (index == "") {
+        throw new Error("Index is required");
+      }
 
-//       if (Object.keys(data).length == 0) {
-//         throw new Error("Data is required");
-//       }
+      if (Object.keys(data).length == 0) {
+        throw new Error("Data is required");
+      }
 
-//       let result = 
-//       await elasticsearch.client.update({
-//         id : id,
-//         index : index,
-//         body : {
-//           doc : data ,
-//           doc_as_upsert : true
-//         }
-//       });
+      let result = 
+      await elasticsearch.client.update({
+        id : id,
+        index : index,
+        body : {
+          doc : data ,
+          doc_as_upsert : true
+        }
+      });
 
-//       return resolve(result);
+      return resolve(result);
 
-//     } catch (error) {
-//       return reject(error);
-//     }
-//   })
-// };
+    } catch (error) {
+      return reject(error);
+    }
+  })
+};
 
-// module.exports = {
-//   get : get,
-//   createOrUpdate : createOrUpdate
-// };
+module.exports = {
+  get : get,
+  createOrUpdate : createOrUpdate
+};
