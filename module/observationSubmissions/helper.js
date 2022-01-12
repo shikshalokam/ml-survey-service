dd0f1b421684f8ec53723e912130de4ce5a032e7
rakeshSgr <rakesh.k@pacewisdom.com>
2022-01-12 13:39:14 +0530

    rate submission api change

--- module/observationSubmissions/helper.js
+++ module/observationSubmissions/helper.js
@@ -329,7 +329,7 @@ module.exports = class ObservationSubmissionsHelper {
                                     if (question.responseType == "multiselect") {
                                         questionMaxScore += option.score;
                                     }
-                                    (option.score && option.score > 0) ? submissionDocument.questionDocuments[question._id.toString()][`${option.value}-score`] = option.score : "";
+                                    (option.score && option.score >= 0) ? submissionDocument.questionDocuments[question._id.toString()][`${option.value}-score`] = option.score : "";
                                 })
                             }
                             if (question.sliderOptions && question.sliderOptions.length > 0) {
