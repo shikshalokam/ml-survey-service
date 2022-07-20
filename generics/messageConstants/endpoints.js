df333eca26282980fa8e9da7174f0df734ee0b98
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-07-20 10:32:21 +0530

    user resd added

--- generics/messageConstants/endpoints.js
+++ generics/messageConstants/endpoints.js
@@ -17,6 +17,7 @@ module.exports = {
     SOLUTION_DETAILS_BASED_ON_ROLE_LOCATION : "/v1/solutions/detailsBasedOnRoleAndLocation",
     GET_PROJECT_TEMPLATE_LISTS : "/v1/project/templates/listByIds",
     DOWNLOADABLE_FILE_URL: "/v1/cloud-services/files/getDownloadableUrl",
+    USER_READ : "/user/v1/read",
     GET_LOCATION_DATA : "/api/data/v1/location/search",
     GET_SCHOOL_DATA : "/api/org/v1/search"
 }
