7d56fdce8539a9923c7df05b0d1e7c2138f74691
ankitshahu <ankit@matchlog.delivery>
2023-11-20 17:59:52 +0530

    updated PR

--- config/kafkaConfig.js
+++ config/kafkaConfig.js
@@ -1,42 +1,39 @@
 //dependencies
-const kafka = require('kafka-node')
+const kafka = require("kafka-node");
 const SUBMISSION_RATING_QUEUE_TOPIC = process.env.SUBMISSION_RATING_QUEUE_TOPIC;
 const USER_DELETE_TOPIC = process.env.USER_DELETE_TOPIC;
-var connect = function() {
+var connect = function () {
+  Producer = kafka.Producer;
+  KeyedMessage = kafka.KeyedMessage;
+  const Consumer = kafka.Consumer;
+  client = new kafka.KafkaClient({
+    kafkaHost: process.env.KAFKA_URL,
+  });
 
-    Producer = kafka.Producer
-    KeyedMessage = kafka.KeyedMessage
-    client = new kafka.KafkaClient({
-      kafkaHost : process.env.KAFKA_URL
-    })
+  client.on("error", function (error) {
+    console.error.bind(console, "kafka connection error!");
+  });
 
-    client.on('error', function(error) {
-      console.error.bind(console, "kafka connection error!")
-    });
+  producer = new Producer(client);
 
-    producer = new Producer(client)
+  producer.on("ready", function () {
+    console.log("Connected to Kafka");
+  });
 
-    producer.on('ready', function () {
-      console.log("Connected to Kafka");
-    });
-   
-    producer.on('error', function (err) {
-      console.error.bind(console, "kafka producer creation error!")
-    })
-
-   
+  producer.on("error", function (err) {
+    console.error.bind(console, "kafka producer creation error!");
+  });
 
   _sendToKafkaConsumers(SUBMISSION_RATING_QUEUE_TOPIC, process.env.KAFKA_URL)
 
 
   _sendToKafkaConsumers(USER_DELETE_TOPIC, process.env.KAFKA_URL)
 
-    return {
-      kafkaProducer: producer,
-      kafkaClient: client,
-      kafkaKeyedMessage: KeyedMessage
-    };
-
+  return {
+    kafkaProducer: producer,
+    kafkaClient: client,
+    kafkaKeyedMessage: KeyedMessage,
+  };
 };
 
 
@@ -50,42 +47,42 @@ var connect = function() {
 
  var _sendToKafkaConsumers = function (topic, host) {
 
-  if(topic && topic != "OFF" ){
-    let consumer = new kafka.ConsumerGroup(
-      {
-        kafkaHost: host,
-        groupId: process.env.KAFKA_GROUP_ID,
-        autoCommit: true,
-      },
-      topic
-    );
-    consumer.on("message", async function (message) {
-      console.log("-------Kafka consumer log starts here------------------");
-      console.log("Topic Name: ", topic);
-      console.log("Message: ", JSON.stringify(message));
-      console.log("-------Kafka consumer log ends here------------------");
+    if(topic && topic != "OFF" ){
+      let consumer = new kafka.ConsumerGroup(
+        {
+          kafkaHost: host,
+          groupId: process.env.KAFKA_GROUP_ID,
+          autoCommit: true,
+        },
+        topic
+      );
+      consumer.on("message", async function (message) {
+        console.log("-------Kafka consumer log starts here------------------");
+        console.log("Topic Name: ", topic);
+        console.log("Message: ", JSON.stringify(message));
+        console.log("-------Kafka consumer log ends here------------------");
 
-      if (message && message.topic === SUBMISSION_RATING_QUEUE_TOPIC) {
-        submissionRatingQueueConsumer.messageReceived(message);
-      }
+        if (message && message.topic === SUBMISSION_RATING_QUEUE_TOPIC) {
+          submissionRatingQueueConsumer.messageReceived(message);
+        }
 
-      // call userDelete consumer
-      if (message && message.topic === USER_DELETE_TOPIC) {
-        userDMSConsumer.messageReceived(message);
-      }
-    });
+        // call userDelete consumer
+        if (message && message.topic === USER_DELETE_TOPIC) {
+          userDeleteConsumer.messageReceived(message);
+        }
+      });
 
-    consumer.on("error", async function (error) {
+      consumer.on("error", async function (error) {
 
-      if (error.topics && error.topics[0] === SUBMISSION_RATING_QUEUE_TOPIC) {
-        submissionRatingQueueConsumer.errorTriggered(error);
-      }
+        if (error.topics && error.topics[0] === SUBMISSION_RATING_QUEUE_TOPIC) {
+          submissionRatingQueueConsumer.errorTriggered(error);
+        }
 
-      if (error.topics && error.topics[0] === USER_DELETE_TOPIC) {
-        userDMSConsumer.errorTriggered(error);
-      }
-    });
-  }
-};
+        if (error.topics && error.topics[0] === USER_DELETE_TOPIC) {
+          userDeleteConsumer.errorTriggered(error);
+        }
+      });
+    }
+  };
 
 module.exports = connect;
